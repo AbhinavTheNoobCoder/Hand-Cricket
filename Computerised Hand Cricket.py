df5d7ad6beaa785217c6404eb40b15aaba511f5f
Abhinav <96286033+AbhinavTheNoobCoder@users.noreply.github.com>
2024-06-09 17:22:38 +0530

    Update Computerised Hand Cricket.py

--- Computerised Hand Cricket.py
+++ Computerised Hand Cricket.py
@@ -2,10 +2,19 @@ import random
 from math import floor
 print('''Welcome to Hand Cricket.
 You have to write the playing XI for both the teams and the computer will play the game.
-When you are writing the name of an all-rounder or bowler, please write "(b)" AFTER their name.
-However, when you are writing the captain name, DO NOT write "(b)"!!
+When you are writing the name of an all-rounder or bowler, write "(b)" AFTER their name.
+When you are writing the name of the captain, write "(c)" AFTER their name.
+When you are writing the name of the wicketkeeper, write "(wk)" AFTER their name.
+The 3 specifications can be written in any order.
 ''')
 
+class Player():
+  def __init__(self, name):
+    self.name = name
+    self.bat_runs = self.bat_balls = self.bowl_runs = self.bowl_balls = 0
+    self.wickets = 0
+    self.did_bat = False
+    self.dismissal = "not out"
 
 #creating CricketTeam to store all info related to the team
 class CricketTeam():
@@ -13,22 +22,28 @@ class CricketTeam():
     self.name = name
     self.playing_xi = []
     self.bowlers = []
-    self.captain = None
-    self.wk = None
+    self.wickets_lost = self.score = self.balls_played = 0
   
   def initialiseTeam(self):
     for _ in range(11):
-      player_name = input(f"Enter a player's name for {self.name}: ").strip(" ")
+      player = Player(input(f"Enter a player's name for {self.name}: ").strip(" "))
 
-      if "(b)" in player_name or "(B)" in player_name:
-        player_name = player_name.removesuffix("(b)")
-        player_name = player_name.removesuffix("(B)")
-        self.bowlers.append(player_name)
+      if "(c)" in player.name or "(c)" in player.name:
+        player.name = player.name.replace("(c)", "")
+        player.name = player.name.replace("(C)", "")
+        self.captain = player
       
-      self.playing_xi.append(player_name)
-    
-    self.captain = input(f"Enter the captain for {self.name}: ")
-    self.wk = input(f"Enter the wicketkeeper for {self.name}: ")
+      if "(WK)" in player.name or "(wk)" in player.name:
+        player.name = player.name.replace("(WK)", "")
+        player.name = player.name.replace("(wk)", "")
+        self.wk = player
+      
+      if "(b)" in player.name or "(B)" in player.name:
+        player.name = player.name.replace("(b)", "")
+        player.name = player.name.replace("(B)", "")
+        self.bowlers.append(player)
+      
+      self.playing_xi.append(player)
 
 #accepting two teams
 home_team = CricketTeam(input("Enter the home team: ").strip(" "))
@@ -128,54 +143,14 @@ def createBowlingOrder(bowler_list: list):
 #defining batting
 def batting(batting_side: CricketTeam, bowling_side: CricketTeam, chasing: bool, target: int | None = None):
   bat_partners = [batting_side.playing_xi[0], batting_side.playing_xi[1]]
+  for _ in bat_partners:
+    _.did_bat = True
+
   available_batters = batting_side.playing_xi[2: ]
-  batter_runs = {}.fromkeys(bat_partners, 0)
-  batter_balls = {}.fromkeys(bat_partners, 0)
-  batter_dismissals = {}
 
   bowling_order = createBowlingOrder(bowling_side.bowlers)
-  bowler_wickets = {}.fromkeys(bowling_side.bowlers, 0)
-  bowler_runs = {}.fromkeys(bowling_side.bowlers, 0)
-  bowler_balls = {}.fromkeys(bowling_side.bowlers, 0)
 
-  balls_played = team_score = wickets_lost = 0
-
-  def createScorecard():
-    scorecard = f"{batting_side.name} - {team_score}/{wickets_lost} ({balls_played//6}.{balls_played%6} overs):"
-    for batter in batter_runs:
-      name = batter
-      batter_score = f"{batter_runs[batter]}({batter_balls[batter]})"
-      if batter in bat_partners:
-        name += '* '
-      
-      if batter == batting_side.captain:
-        name += "(c)"
-
-      if batter == batting_side.wk:
-        name += "(wk)"
-
-      if batter in batter_dismissals:
-        scorecard += f'\n{name:<25}{batter_dismissals[batter]:<40}{batter_score:>5}'
-
-      else:
-        dismissal = 'not out'
-        scorecard += f'\n{name:<25}{dismissal:<40}{batter_score:>5}'
-
-    
-    scorecard += f'\n\n{bowling_side.name} bowling:'
-    for bowler in bowler_wickets:
-      name = bowler
-      overs = f'{bowler_balls[bowler] // 6}.{bowler_balls[bowler] % 6} overs'
-      figures = f'{bowler_wickets[bowler]}-{bowler_runs[bowler]}'
-      if bowler == bowling_side.captain:
-        name += '(c)'
-
-      if bowler_balls[bowler] != 0:
-        scorecard += f'\n{name:<25}{figures} ({overs})'
-
-    return scorecard
-
-  while balls_played < 60:
+  while batting_side.balls_played < 60:
     #playing an over
     for _ in range(6):
       batter_on_strike = bat_partners[0]
@@ -183,97 +158,119 @@ def batting(batting_side: CricketTeam, bowling_side: CricketTeam, chasing: bool,
 
       bat_number = random.choice(numbers)
       bowl_number = random.choice(numbers)
-      batter_balls[batter_on_strike] += 1 #incrementing the ball already
-      bowler_balls[bowler] += 1
-      balls_played += 1
+      batter_on_strike.bat_balls += 1 #incrementing the ball already
+      bowler.bowl_balls += 1
+      batting_side.balls_played += 1
 
       if bat_number == bowl_number: #wicket
-        wickets_lost += 1
+        batting_side.wickets_lost += 1
         dismissal = common_dismissal_types[floor(random.triangular(0, 2, 0))]
-        bowler_wickets[bowler] += 1
+        bowler.wickets += 1
         bat_partners.remove(batter_on_strike)
 
         if dismissal == 'c X b Y':
           catcher = random.choice(bowling_side.playing_xi)
           if catcher == bowler:
-            dismissal = f'c & b {bowler}'
+            dismissal = f'c & b {bowler.name}'
 
           else:
-            dismissal = f'c {catcher}  b {bowler}'
+            dismissal = f'c {catcher.name}  b {bowler.name}'
 
         elif dismissal == 'b':
-          dismissal = f'b {bowler}'
+          dismissal = f'b {bowler.name}'
 
         elif dismissal == 'lbw':
-          dismissal = f'lbw {bowler}'
+          dismissal = f'lbw {bowler.name}'
           
-        batter_dismissals[batter_on_strike] = dismissal
+        batter_on_strike.dismissal = dismissal
 
       else: #not a wicket
         runs_scored = scoreCounting(bat_number, bowl_number)
-        batter_runs[batter_on_strike] += runs_scored
-        team_score += runs_scored
-        bowler_runs[bowler] += runs_scored
+        batter_on_strike.bat_runs += runs_scored
+        batting_side.score += runs_scored
+        bowler.bowl_runs += runs_scored
 
         if runs_scored % 2 == 1:
           bat_partners.reverse()
       
       if chasing:
-        if team_score >= target: #chased the target
-          result = f"{batting_side.name} beat {bowling_side.name} by {10 - wickets_lost} wickets."
-          scorecard = createScorecard()
-          return result, scorecard
+        if batting_side.score >= target: #chased the target
+          result = f"{batting_side.name} beat {bowling_side.name} by {10 - batting_side.wickets_lost} wickets."
+          return result
 
-      if wickets_lost == 10: #all out
+      if batting_side.wickets_lost == 10: #all out
         if not chasing: #first batting
-          scorecard = createScorecard()
-          return team_score + 1, scorecard   #target
+          return batting_side.score + 1   #target
         
         elif chasing: #couldn't chase the target and got all out
-          if team_score < target - 1:
-            scorecard = createScorecard()     
-            result = f"{bowling_side.name} beat {batting_side.name} by {target - 1 - team_score} runs."
+          if batting_side.score < target - 1: #lesser runs than opponent
+            result = f"{bowling_side.name} beat {batting_side.name} by {target - 1 - batting_side.score} runs."
           
-          elif team_score == target - 1:
-            scorecard = createScorecard()
+          elif batting_side.score == target - 1: #same runs as the opponent
             result = "Match drawn."
 
-          return result, scorecard
+          return result
 
         break
 
       if len(bat_partners) == 1:
-        new_batter = available_batters[0]
-        available_batters.pop(0)
+        new_batter = available_batters.pop(0)
         bat_partners.insert(0, new_batter)
-        batter_runs[new_batter] = batter_balls[new_batter] = 0
+        new_batter.did_bat = True
 
     bat_partners.reverse() #the over concluded so the strike switches
     bowling_order.pop(0) #next bowler will be upfront
 
   else: #didn't get all out
     if not chasing:
-      scorecard = createScorecard()
-      return team_score + 1, scorecard
+      return batting_side.score + 1 #target
     
     elif chasing: #couldn't chase and didn't get all out
-      if team_score < target - 1:
-        result = f"{bowling_side.name} beat {batting_side.name} by {target - 1 - team_score} runs."
-        scorecard = createScorecard()
-      
-      elif team_score == target - 1:
+      if batting_side.score < target - 1:
+        result = f"{bowling_side.name} beat {batting_side.name} by {target - 1 - batting_side.score} runs."
+
+      elif batting_side.score == target - 1:
         result = "Match drawn."
-        scorecard = createScorecard()
-        
-      return result, scorecard
+
+      return result
 
 defending_team = toss_dict['bat']
 chasing_team = toss_dict['bowl']
-target, first_scorecard = batting(defending_team, chasing_team, False)
-result, second_scorecard = batting(chasing_team, defending_team, True, target)
+target = batting(defending_team, chasing_team, False)
+result = batting(chasing_team, defending_team, True, target)
+
+def createScorecard(batting_side: CricketTeam, bowling_side: CricketTeam):
+  scorecard = f"{batting_side.name} - {batting_side.score}/{batting_side.wickets_lost} ({batting_side.balls_played//6}.{batting_side.balls_played%6} overs):"
+  for batter in batting_side.playing_xi:
+    if batter.did_bat:
+      name = batter.name
+      batter_score = f"{batter.bat_runs}({batter.bat_balls})"
+      if batter.dismissal == "not out":
+        name += '* '
+    
+      if batter == batting_side.captain:
+        name += "(c)"
+
+      if batter == batting_side.wk:
+        name += "(wk)"
+
+      scorecard += f'\n{name:<25}{batter.dismissal:<50}{batter_score:>5}'
+
+  scorecard += f'\n\n{bowling_side.name} bowling:'
+  for bowler in bowling_side.bowlers:
+    name = bowler.name
+    overs = f'{bowler.bowl_balls // 6}.{bowler.bowl_balls % 6} overs'
+    figures = f'{bowler.wickets}-{bowler.bowl_runs}'
+    if bowler == bowling_side.captain:
+      name += '(c)'
+
+    if bowler.bowl_balls != 0:
+      scorecard += f'\n{name:<25}{figures} ({overs})'
+
+  print(scorecard, '\n\n')
 
 print('\n\n')
 print(toss_statement)
 print("Result:", result, '\n')
-print(first_scorecard, '\n\n')
-print(second_scorecard)
+createScorecard(defending_team, chasing_team)
+createScorecard(chasing_team, defending_team)
